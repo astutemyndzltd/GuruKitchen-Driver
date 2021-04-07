29e1a66c0a5bded6e3d77b1f7c29bf42bcd68f22
Anik Banerjee <anik@astutemyndz.com>
2021-04-07 01:03:47 -0700

    Initial Commit

---
+++ android/app/src/main/kotlin/com/gurukitchenam/driver/Application.kt
@@ -0,0 +1,22 @@
+package com.gurukitchenam.driver
+
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+
+class Application : FlutterApplication(), PluginRegistrantCallback {
+
+    override fun onCreate() {
+        super.onCreate()
+        FlutterFirebaseMessagingService.setPluginRegistrant(this)
+    }
+
+    override fun registerWith(registry: PluginRegistry?) {
+        FirebaseCloudMessagingPluginRegistrant.registerWith(registry)
+        //FirebaseMessagingPlugin.registerWith(registry?.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"))
+    }
+
+}
